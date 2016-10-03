75fd574817ee79ff9bed42894e6c0c8354c57f70
shway250 <jackson_duhon@yahoo.com>
2016-10-03 10:51:11 -0700

    making initial commit

---
+++ js/sketch.js
@@ -0,0 +1,19 @@
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+
+// Example 1-1: stroke and fill
+
+function setup() {
+  createCanvas(480, 270);
+  background(0);
+}
+
+function draw() {
+  if(mouseIsPressed){
+  	fill(0);
+  } else{
+  	fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
